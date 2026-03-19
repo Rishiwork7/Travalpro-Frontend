071c7733918a84fde7962d20f35763a903e45255
Akshit Gupta <Akshitgupta4857@gmail.com>
2026-03-19 11:57:49 +0530

    fix: update Dubai deal image URL

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -43,7 +43,7 @@ export default function Home() {
       price: 295,
       tags: ["Direct Flight", "Refundable"],
       image:
-        "https://images.unsplash.com/photo-1518684079105-312e97e1743f?q=80&w=800&auto=format&fit=crop",
+        "https://images.unsplash.com/photo-1546412414-8035e1776c9a?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       from: "BOM",
